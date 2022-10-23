be036d0c8334459fe6c29483f76f19f405005036
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2022-10-23 22:29:50 +0100

    JavaScript

---
+++ Pam Jav/funtion.js
@@ -0,0 +1,15 @@
+//alert("connected");
+"use strict"; 
+
+function sayHello(){
+    console.log("Hello!"); 
+}
+
+console.log("starting");
+sayHello();
+console.log("done");
+console.log("Happy Day")
+function sayHello2(yourname){
+    console.log("Hulala " + yourname + "!")
+}
+sayHello2("Desmond");
